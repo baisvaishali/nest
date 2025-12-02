e82336d16abf2f45df27b2a2b85a9843486f0d6c
vaishali <vaishali.bais@nliven.co.in>
2025-12-02 19:17:35 +0530

    removed db

--- src/right/right.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('right')
-export class RightController {}
--- src/right/right.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RightController } from './right.controller';
-import { RightService } from './right.service';
-
-@Module({
-  controllers: [RightController],
-  providers: [RightService]
-})
-export class RightModule {}
