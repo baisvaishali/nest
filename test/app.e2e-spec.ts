9949b68972fde4a0b8f16be00944a5774f16c46c
vaishali <vaishali.bais@nliven.co.in>
2025-12-08 20:57:40 +0530

    hello world testing 2

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World! Vaishali Bais');
+      .expect('Hello World! Vais');
   });
 });
